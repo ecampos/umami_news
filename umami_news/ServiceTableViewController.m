224fceb7c3d0dde72064a73af26880e82c84d067
ecampos <campos.e@gmail.com>
2013-01-18 17:38:50 +0100

    working navigation and clean code

--- umami_news/ServiceTableViewController.m
+++ umami_news/ServiceTableViewController.m
@@ -30,16 +30,13 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    
     Service *facebook = [[Service alloc] initServiceName:@"facebook" serviceTitle:@"facebook"];//  resultDictionary:(NSDictionary *)
     Service *twitter = [[Service alloc] initServiceName:@"twitter" serviceTitle:@"twitter"];
     Service *news = [[Service alloc] initServiceName:@"news" serviceTitle:@"news"];
     
-       self.services = [NSArray arrayWithObjects:facebook, twitter, news, nil];
-    // Uncomment the following line to preserve selection between presentations.
-    // self.clearsSelectionOnViewWillAppear = NO;
- 
-    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+    self.services = [NSArray arrayWithObjects:facebook, twitter, news, nil];
+
 }
 
 - (void)didReceiveMemoryWarning
@@ -64,56 +61,18 @@
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
+
     static NSString *CellIdentifier = @"Cell";
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier forIndexPath:indexPath];
+   
     Service *s = [services objectAtIndex:indexPath.row];
     cell.textLabel.text = s.serviceTitle;
-//    cell.textLabel.backgroundColor = [UIColor blueColor];
-    cell.textLabel.textColor = [UIColor colorWithRed:219/255.0f green:205/255.0f blue:166/255.0f alpha:1.0];
-    
-    tableView.backgroundColor =[UIColor colorWithRed:91/255.0f green:160/255.0f blue:184/255.0f alpha:2.0];
-    
+    cell.backgroundColor =[UIColor colorWithRed:236/255.0f green:236/255.0f blue:231/255.0f alpha:1.0];
+    cell.textLabel.textColor = [UIColor colorWithRed:15/255.0f green:61/255.0f blue:72/255.0f alpha:1.0];
     return cell;
 }
 
-/*
-// Override to support conditional editing of the table view.
-- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    // Return NO if you do not want the specified item to be editable.
-    return YES;
-}
-*/
 
-/*
-// Override to support editing the table view.
-- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    if (editingStyle == UITableViewCellEditingStyleDelete) {
-        // Delete the row from the data source
-        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
-    }   
-    else if (editingStyle == UITableViewCellEditingStyleInsert) {
-        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-    }   
-}
-*/
-
-/*
-// Override to support rearranging the table view.
-- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath
-{
-}
-*/
-
-/*
-// Override to support conditional rearranging of the table view.
-- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    // Return NO if you do not want the item to be re-orderable.
-    return YES;
-}
- */ 
 -(void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender{
     if ([segue.identifier isEqualToString:@"goToPreview"]) {
         UITableViewCell *cell =(UITableViewCell *)sender;
@@ -127,16 +86,5 @@
   
 
 #pragma mark - Table view delegate
-/*
-- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    // Navigation logic may go here. Create and push another view controller.
-    
-     PreviewTableViewController *preview = [[PreviewTableViewController alloc] initWithNibName:@"PreviewTableViewController" bundle:nil];
-     // ...
-     // Pass the selected object to the new view controller.
-     [self.navigationController pushViewController:preview animated:YES];
-     
-}
-*/
+
 @end
